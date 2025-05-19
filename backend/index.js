7336e93c8e18164bc7a8b763ab195269341b0744
Ihab Sbeih <isbeih@gi.mshome.net>
2025-05-19 23:53:15 +0000

    Setup backend server with Express

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "Hello World!" });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
