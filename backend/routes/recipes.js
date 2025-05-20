8d4fbdffdfaf4c1c73d3c318b474bb928f467734
i-sbe <ihab_sbeih@hotmail.com>
2025-05-20 21:41:16 +0000

    feat(api): implement POST /api/recipes to add new recipe.

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -3,6 +3,8 @@ import {pool} from '../db/db.js';
 
 const router = express.Router();
 
+//--------------------------------------------------------------------------
+
 router.get('/', async (req, res) => {
   try {
     const rows = await pool.query('SELECT * FROM recipes');
@@ -10,6 +12,31 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Fehler beim Abrufen der Rezepte' });
+  }
+});
+
+//--------------------------------------------------------------------------
+
+router.post('/', async (req, res) => {
+  const { user_id, title, ingredients, steps } = req.body;
+
+  if (!user_id || !title || !ingredients || !steps) {
+    return res.status(400).json({ message: 'Alle Felder sind erforderlich.' });
+  }
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO recipes (user_id, title, ingredients, steps) VALUES (?, ?, ?, ?)',
+      [user_id, title, ingredients, steps]
+    );
+
+    res.status(201).json({
+      message: 'Rezept wurde erfolgreich erstellt.',
+      id: Number(result.insertId)
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Fehler beim Erstellen des Rezepts.' });
   }
 });
 
